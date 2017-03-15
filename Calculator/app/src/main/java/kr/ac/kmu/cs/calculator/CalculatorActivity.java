d3d5b1917d57576a3e706da605cd838ffa0f67cb
JIHYEPYO <pyojihye95@gmail.com>
2017-03-15 12:38:59 +0900

    2017/03/15

--- Calculator/app/src/main/java/kr/ac/kmu/cs/calculator/CalculatorActivity.java
+++ Calculator/app/src/main/java/kr/ac/kmu/cs/calculator/CalculatorActivity.java
@@ -30,54 +30,54 @@ public class CalculatorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
 
-        button1=(Button)findViewById(R.id.button1);
-        button2=(Button)findViewById(R.id.button2);
-        button3=(Button)findViewById(R.id.button3);
-        button4=(Button)findViewById(R.id.button4);
-        button5=(Button)findViewById(R.id.button5);
-        button6=(Button)findViewById(R.id.button6);
-        button7=(Button)findViewById(R.id.button7);
-        button8=(Button)findViewById(R.id.button8);
-        button9=(Button)findViewById(R.id.button9);
-        button0=(Button)findViewById(R.id.button0);
-        buttonClear=(Button)findViewById(R.id.buttonClear);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
+        button9 = (Button) findViewById(R.id.button9);
+        button0 = (Button) findViewById(R.id.button0);
+        buttonClear = (Button) findViewById(R.id.buttonClear);
 
-        editTextOK=(EditText)findViewById(R.id.editTextOk);
-        buttonOK=(Button)findViewById(R.id.buttonOk);
+        editTextOK = (EditText) findViewById(R.id.editTextOk);
+        buttonOK = (Button) findViewById(R.id.buttonOk);
     }
 
-    public void onButtonNumberClick(View v){
+    public void onButtonNumberClick(View v) {
         String edit = editTextOK.getText().toString();
-        switch (((Button) v).getText().toString()){
+        switch (((Button) v).getText().toString()) {
             case "1":
-                editTextOK.setText(edit+Integer.parseInt(button1.getText().toString()));
+                editTextOK.setText(edit + Integer.parseInt(button1.getText().toString()));
                 break;
             case "2":
-                editTextOK.setText(edit+Integer.parseInt(button2.getText().toString()));
+                editTextOK.setText(edit + Integer.parseInt(button2.getText().toString()));
                 break;
             case "3":
-                editTextOK.setText(edit+Integer.parseInt(button3.getText().toString()));
+                editTextOK.setText(edit + Integer.parseInt(button3.getText().toString()));
                 break;
             case "4":
-                editTextOK.setText(edit+Integer.parseInt(button4.getText().toString()));
+                editTextOK.setText(edit + Integer.parseInt(button4.getText().toString()));
                 break;
             case "5":
-                editTextOK.setText(edit+Integer.parseInt(button5.getText().toString()));
+                editTextOK.setText(edit + Integer.parseInt(button5.getText().toString()));
                 break;
             case "6":
-                editTextOK.setText(edit+Integer.parseInt(button6.getText().toString()));
+                editTextOK.setText(edit + Integer.parseInt(button6.getText().toString()));
                 break;
             case "7":
-                editTextOK.setText(edit+Integer.parseInt(button7.getText().toString()));
+                editTextOK.setText(edit + Integer.parseInt(button7.getText().toString()));
                 break;
             case "8":
-                editTextOK.setText(edit+Integer.parseInt(button8.getText().toString()));
+                editTextOK.setText(edit + Integer.parseInt(button8.getText().toString()));
                 break;
             case "9":
-                editTextOK.setText(edit+Integer.parseInt(button9.getText().toString()));
+                editTextOK.setText(edit + Integer.parseInt(button9.getText().toString()));
                 break;
             case "0":
-                editTextOK.setText(edit+Integer.parseInt(button0.getText().toString()));
+                editTextOK.setText(edit + Integer.parseInt(button0.getText().toString()));
                 break;
             case "clear":
                 editTextOK.setText("");
--- Calculator/app/src/main/java/kr/ac/kmu/cs/calculator/MainActivity.java
+++ Calculator/app/src/main/java/kr/ac/kmu/cs/calculator/MainActivity.java
@@ -23,33 +23,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonPlus=(Button)findViewById(R.id.buttonPlus);
-        buttonMinus=(Button)findViewById(R.id.buttonMinus);
-        buttonMul=(Button)findViewById(R.id.buttonMul);
-        buttonDiv=(Button)findViewById(R.id.buttonDiv);
+        buttonPlus = (Button) findViewById(R.id.buttonPlus);
+        buttonMinus = (Button) findViewById(R.id.buttonMinus);
+        buttonMul = (Button) findViewById(R.id.buttonMul);
+        buttonDiv = (Button) findViewById(R.id.buttonDiv);
 
-        editTextNum1=(EditText)findViewById(R.id.editTextNum1);
-        editTextNum2=(EditText)findViewById(R.id.editTextNum2);
-        editTextResult=(EditText)findViewById(R.id.editTextResult);
+        editTextNum1 = (EditText) findViewById(R.id.editTextNum1);
+        editTextNum2 = (EditText) findViewById(R.id.editTextNum2);
+        editTextResult = (EditText) findViewById(R.id.editTextResult);
 
     }
 
-    public void onButtonClick(View v){
+    public void onButtonClick(View v) {
         int num1 = Integer.parseInt(editTextNum1.getText().toString());
         int num2 = Integer.parseInt(editTextNum2.getText().toString());
 
-        switch (((Button) v).getText().toString()){
+        switch (((Button) v).getText().toString()) {
             case "+":
-                editTextResult.setText(Integer.toString(num1+num2));
+                editTextResult.setText(Integer.toString(num1 + num2));
                 break;
             case "-":
-                editTextResult.setText(Integer.toString(num1-num2));
+                editTextResult.setText(Integer.toString(num1 - num2));
                 break;
             case "*":
-                editTextResult.setText(Integer.toString(num1*num2));
+                editTextResult.setText(Integer.toString(num1 * num2));
                 break;
             case "/":
-                editTextResult.setText(Integer.toString(num1/num2));
+                editTextResult.setText(Integer.toString(num1 / num2));
                 break;
         }
     }
